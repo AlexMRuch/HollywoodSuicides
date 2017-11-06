86a0634a649ad959670b7b521c5181eb52d45e49
AlexMRuch <amr442@cornell.edu>
2017-11-06 10:45:18 -0500

    Updated headers of extractor.py and SeqTests.R

--- SeqTests.R
+++ SeqTests.R
@@ -1,1 +1,19 @@
+# SeqTests.R
+# Written by Alexander M. Ruch (amr442@cornell.edu)
+# Co-authors: Reid Ralston, Benjamin Cornwell
+# Last update: November 6, 2017
+
+# Stage 2 deliverables:
+# TODO: Determine sequence analyses plan and extracts
+# TODO: Determine subsequent analyses using sequence extracts
+# TODO: Determine primary analyses
+# TODO: Determine secondary analyses
+# TODO: Determine assumptions for all analyses
+#
+# Stage 3 deliverables:
+# TODO: Run, review, and refine primary analyses
+# TODO: Run, review, and refine secondary analyses
+# TODO: Generate outputs: figures, visualizations, etc.
+
+
 import TraMineR
--- extractor.py
+++ extractor.py
@@ -3,5 +3,9 @@
 # Co-authors: Reid Ralston, Benjamin Cornwell
 # Last update: November 6, 2017
 
-# TODO:
-# TODO: 
+# Stage 1 deliverables:
+# TODO: Extract Dataset A: actor/actress data
+# TODO: Extract Dataset M: movie data
+# TODO: Identify and extract Dataset O: other relevant data
+# TODO: Determine data structure(s)
+# TODO: Create Joined Dataset: joined Datasets A, M, O
